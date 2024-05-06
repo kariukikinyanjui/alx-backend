5827cc876eb91987dd4b9e40632397f9a1574a8c
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-06 21:03:46 +0000

    [feat]: background workers

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', function() {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', function(err) {
+    console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', function(channel, message) {
+    console.log(message);
+    if (message === 'KILL_SERVER') {
+	client.unsubscribe();
+	client.quit();
+    }
+});
